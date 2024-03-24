8253868aa606e3b59cf65cc89176af282052db48
tajtanvvir1 <tanvirah1223@gmail.com>
2024-03-24 07:56:51 +0600

    Category list create

---
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -0,0 +1,10 @@
+const FeaturedJobs = () => {
+   return (
+      <div>
+        <h2 className="text-5xl">Featured Jobs</h2> 
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Error inventore blanditiis aliquid maiores sapiente soluta!</p>
+      </div>
+   );
+};
+
+export default FeaturedJobs;
